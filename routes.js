49cb7ae27b80cfdc6f3d5c0e51afe50c4c6559bd
Theo L <atlufkin@outlook.com>
2022-02-11 11:39:26 -0500

    setting up db sync

--- routes.js
+++ routes.js
@@ -20,9 +20,25 @@ router.get('/users', authenticateUser, asyncHandler (async (req, res) => {
     res.json(user);
 }));
 
+//Create a new user
+router.post('/users', asyncHandler(async (req, res) => {
+    try {
+      await User.create(req.body);
+      res.status(201).json({ "message": "Account successfully created!" });
+    } catch (error) {
+      console.log('ERROR: ', error.name);
+  
+      if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+        const errors = error.errors.map(err => err.message);
+        res.status(400).json({ errors });   
+      } else {
+        throw error;
+      }
+    }
+  }));
 //COURSE ROUTES
 
-//return all courses including the user associated with the course.
+return all courses including the user associated with the course.
 router.get('/courses', asyncHandler(async (req, res) => {
     const courses = await Course.findAll({
         include: {
