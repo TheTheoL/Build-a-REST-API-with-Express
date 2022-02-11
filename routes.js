5e8fcee61e4523cb01d30f90603509fcdd15d124
Theo L <atlufkin@outlook.com>
2022-02-11 13:53:22 -0500

    setting up db sync

--- routes.js
+++ routes.js
@@ -76,21 +76,7 @@ router.post('/courses', authenticateUser, asyncHandler (async (req, res) => {
     res.status(201).location(`/courses/${course.id}`).end();
 }));
 
-//Route that will update the corresponding course and return a 204 HTTP status code and no content.
-router.put('/courses/:id', authenticateUser,
-asyncHandler(async (req, res, next) => {
-  const course = await Course.findByPk(req.params.id);
-    if (!course) {
-        const error = new Error();
-        error.status = 404;
-        error.message = 'Course not found';
-        next(error);
-    } else {
-        await course.update(req.body);
-        res.status(204).end();
-  }
-})
-);
+
 
 
 module.exports = router;
